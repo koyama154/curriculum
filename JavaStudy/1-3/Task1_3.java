f2cc48695b506fc9b10a709bfb78d68909530975
koyama154 <143458494+koyama154@users.noreply.github.com>
2023-10-03 21:48:38 +0900

    1-3課題提出

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,38 @@
+
+public class Task1_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		// ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
+		int plus = 5 + 10;
+
+        // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
+		int minus = 20 - 7;
+
+        // ③下記の処理について、何をしているのかコメントを記入してください。
+        // 10 と 2 を乗算した値を multiply という整数型変数に代入している。
+        int multiply = 10 * 2;
+
+        // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
+        int remi = 20 % 6;
+
+        // ⑤下記の処理について、何をしているのかコメントを記入してください
+        // 中央という値を town 、線という値を line という文字列型変数に代入している。
+        String town = "中央";
+        String line = "線";
+
+        // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
+        String train = town + line;
+
+        // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+ 
+
+	}
+
+}
