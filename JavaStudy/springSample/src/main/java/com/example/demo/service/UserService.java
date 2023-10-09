083ed66064905c6c8666eea0145a91a6a866adbd
koyama154 <143458494+koyama154@users.noreply.github.com>
2023-10-09 12:08:02 +0900

    springSample課題①~⑥

--- JavaStudy/springSample/src/main/java/com/example/demo/service/UserService.java
+++ JavaStudy/springSample/src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.UserRequest;
+import com.example.demo.dto.UserUpdateRequest;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.UserRepository;
 
@@ -28,5 +31,53 @@ public class UserService {
 	 */
 	public List<UserEntity> searchAll() {
 		return userRepository.findAll(); 
+	}
+	
+	/**
+	 * ユーザー情報 主キー検索
+	 * @return  検索結果
+	 */
+	public UserEntity findById(Long id) {
+		return userRepository.findById(id).orElse(null);
+	}
+
+	/**
+	 * ユーザー情報 新規登録
+	 * @param  user ユーザー情報
+	 */
+	public void create(UserRequest userRequest) {
+		Date now = new Date();
+		UserEntity user = new UserEntity();
+		user.setName(userRequest.getName());
+	    user.setAddress(userRequest.getAddress());
+
+
+		user.setPhone(userRequest.getPhone());
+		user.setCreateDate(now);
+		user.setUpdateDate(now);
+		userRepository.save(user);
+	}
+	
+	/**
+	 * ユーザー情報 更新
+	 * @param  user ユーザー情報
+	 */
+	public void update(UserUpdateRequest userUpdateRequest) {
+		UserEntity user = findById(userUpdateRequest.getId());
+	    user.setAddress(userUpdateRequest.getAddress());
+	    user.setName(userUpdateRequest.getName());
+	    user.setPhone(userUpdateRequest.getPhone());
+	    user.setUpdateDate(new Date());
+	    userRepository.save(user);
+	}
+	
+	/**
+	 * ユーザー情報 物理削除
+	 * @param  id ユーザーID
+	 */
+	public void delete(Long id) {
+		//実装2行
+		UserEntity user = findById(id);
+	    userRepository.delete(user);
 	}
 }
